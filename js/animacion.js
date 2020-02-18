f19620d5bf3ff586fa90fd92e86b38c92332a504
Arnaldo Muñoz Mendoza <47994871+ArnyWorld@users.noreply.github.com>
2020-02-18 12:51:19 -0400

    Errores

---
+++ js/animacion.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    $('#click').click(function(){
+       $('.animacion').css('display','block');
+        $('.textCard').css('background','black');    
+        $('h3').css('color','yellow'); 
+        $('p').css('color','yellow'); 
+        $('.animacion2').css('display','block');
+    });
+    
+});
